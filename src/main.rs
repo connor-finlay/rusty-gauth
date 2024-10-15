84bc431ce78eda7bcb82a0c36c5c11941d1cd24c
mopuppet <109947898+mopuppet@users.noreply.github.com>
2024-10-15 11:33:56 +1100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+use base32::Alphabet;
+use base64::{engine::general_purpose::STANDARD, Engine as _};
+use clap::{CommandFactory, Parser};
+use comfy_table::Table;
+use prost::Message;
+use rqrr;
+
+//mod faust;
+
+include!(concat!(env!("OUT_DIR"), "/googleauth.rs"));
+
+/// A command line tool to decode Google Authenticator export data
+#[derive(Parser)]
+struct Args {
+    /// Path to image containing QR-code
+    #[arg(short, long, value_name = "img file")]
+    path: Option<String>,
+
+    /// Migration link decoded from QR-code, starts with: otpauth-migration://offline?data=
+    #[arg(short, long, value_name = "link")]
+    link: Option<String>,
+}
+
+fn deserialise_migration_payload(data: &[u8]) -> Result<MigrationPayload, prost::DecodeError> {
+    MigrationPayload::decode(data)
+}
+
+fn algo_string(algorithm: i32) -> String {
+    match algorithm {
+        1 => String::from("Sha1"),
+        2 => String::from("Sha256"),
+        3 => String::from("Sha512"),
+        4 => String::from("Md5"),
+        _ => String::from("Unspecified"),
+    }
+}
+
+fn algo_type_string(otp_type: i32) -> String {
+    match otp_type {
+        1 => String::from("HOTP"),
+        2 => String::from("TOTP"),
+        _ => String::from("Unspecified"),
+    }
+}
+
+fn digits_count_string(digit_count: i32) -> String {
+    match digit_count {
+        1 => String::from("6"),
+        2 => String::from("8"),
+        _ => String::from("Unspecified"),
+    }
+}
+
+fn get_qrcode_data(path: &str) {
+    let img = image::open(path).expect("Could not open file").to_luma8();
+    let mut img = rqrr::PreparedImage::prepare(img);
+    let grids = img.detect_grids();
+    let (_, content) = grids[0].decode().unwrap();
+    decode_backup(&content);
+}
+
+fn decode_backup(link: &str) {
+    let split = link
+        .strip_prefix("otpauth-migration://offline?data=")
+        .expect("Data is not valid migration data");
+
+    let uri_decoded = urlencoding::decode(&split).expect("Could not url decode data");
+    let base64_decoded = STANDARD
+        .decode(uri_decoded.as_bytes())
+        .expect("Could not base64 decode data");
+    let payload =
+        deserialise_migration_payload(&base64_decoded).expect("Could not deserialise data");
+
+    let mut table = Table::new();
+    table.set_header(vec![
+        "Name",
+        "Issuer",
+        "Secret",
+        "Algorithm",
+        "DigitCount",
+        "OtpType",
+    ]);
+
+    for otp in payload.otp_parameters {
+        table.add_row(vec![
+            otp.name,
+            otp.issuer,
+            base32::encode(Alphabet::Rfc4648 { padding: (false) }, &otp.secret),
+            algo_string(otp.algorithm),
+            digits_count_string(otp.digits),
+            algo_type_string(otp.r#type),
+        ]);
+    }
+    println!("{table}");
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.link {
+        Some(link) => decode_backup(&link),
+        None => match args.path {
+            Some(path) => get_qrcode_data(&path),
+            None => {
+                let mut cmd = Args::command();
+                let _ = cmd.print_long_help();
+                std::process::exit(0);
+                // faust::faust();
+            }
+        },
+    }
+}
